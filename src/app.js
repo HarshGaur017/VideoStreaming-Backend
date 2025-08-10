2aeee21308625cfef15488dca20ff23b1f110318
Harsh Gaur <harshgaur017@gmail.com>
2025-08-10 17:46:50 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -14,4 +14,14 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static('public'))
 app.use(cookieParser())
 
+// Routes Import
+
+import userRouter from './routes/user.routes.js';
+
+// Routes Declaration
+
+app.use("/api/v1/users", userRouter);
+
+// http://localhost:5000/api/v1/users/register
+
 export { app };
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,12 +1,10 @@
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
-    }
-}
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
 
-
-export {asyncHandler}
-
+export { asyncHandler };
 
 // const asyncHandler = () => {}
 // const asyncHandler = (func) => () => {}
@@ -14,8 +12,8 @@ export {asyncHandler}
 
 // const asyncHandler = (fn) => async (req, res, next) => {
 //     try {
-//         await fn(req, res, next); 
+//         await fn(req, res, next);
 //     } catch (error) {
-//      res.status(err.code || 500).json({success: false, message: err.message})  
+//      res.status(err.code || 500).json({success: false, message: err.message})
 //     }
 // }
