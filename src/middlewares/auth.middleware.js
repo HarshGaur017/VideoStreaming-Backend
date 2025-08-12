609c97cef2f450d317bc5076fd2b65252bc0b880
Harsh Gaur <harshgaur017@gmail.com>
2025-08-13 00:21:15 +0530

    login logout controller and middleware added

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,31 @@
+import { ApiError } from "../utils/apiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+
+
+export const verifyJWT = asyncHandler(async(req, res, next) => {
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+        console.log("token", token);
+    
+        if (!token) {
+            throw new ApiError(401, "Unauthorized request");
+        }
+    
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        console.log("decodedToken", decodedToken);
+    
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+        console.log("user", user);
+    
+        if (!user) {
+            throw new ApiError(401, "invalid token");
+        }
+    
+        req.user = user;
+        next();
+    } catch (error) {
+        throw new ApiError(401, "invalid access token");
+    }
+})
