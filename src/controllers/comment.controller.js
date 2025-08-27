d9105acf63e669d958cb3b94dc4656ac21487c50
Harsh Gaur <harshgaur017@gmail.com>
2025-08-27 17:06:52 +0530

    add comment functionality: implement comment routes and controller for managing video comments

---
+++ src/controllers/comment.controller.js
@@ -0,0 +1,161 @@
+import mongoose from "mongoose";
+import { Comment } from "../models/comment.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { Video } from "../models/video.model.js";
+
+const getVideoComments = asyncHandler(async (req, res) => {
+  //TODO: get all comments for a video
+  const { videoId } = req.params;
+  const { page = 1, limit = 10 } = req.query;
+
+  if (!mongoose.Types.ObjectId.isValid(videoId)) {
+    throw new ApiError(400, "invalid video id");
+  }
+
+  const pipeline = [
+    {
+      $match: {
+        video: mongoose.Types.ObjectId(videoId),
+      },
+    },
+    {
+      $sort: {
+        createdAt: -1,
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [
+          {
+            $project: { username: 1, avatar: 1 },
+          },
+        ],
+      },
+    },
+    {
+      $unwind: "$owner",
+    },
+  ];
+
+  const options = {
+    page: Number(page),
+    limit: Number(limit),
+  };
+
+  const comments = await Comment.aggregatePaginate(
+    Comment.aggregate(pipeline),
+    options
+  );
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, comments, "comments fetched successfully"));
+});
+
+const addComment = asyncHandler(async (req, res) => {
+  // TODO: add a comment to a video
+  // take comment from body and video id from params
+  // find video by video id
+  // save comment to db
+  // send response
+
+  const { content } = req.body;
+  const { videoId } = req.params;
+
+  if (!content) {
+    throw new ApiError(400, "content is required");
+  }
+
+  if (!videoId) {
+    throw new ApiError(400, "video id is required");
+  }
+
+  const video = await Video.findById(videoId);
+  if (!video) {
+    return res.status(404).json(new ApiResponse(404, "video not found"));
+  }
+
+  const comment = Comment.create({
+    content,
+    video: videoId,
+    owner: req.user?._id,
+  });
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, comment, "comment created successfully"));
+});
+
+const updateComment = asyncHandler(async (req, res) => {
+  // TODO: update a comment
+  // take comment id from params
+  // take content from body
+  // find comment by id
+  // update comment
+
+  const { commentId } = req.params;
+  const { content } = req.body;
+
+  if (!commentId) {
+    throw new ApiError(400, "comment id is required");
+  }
+
+  if (!content) {
+    throw new ApiError(400, "content is required");
+  }
+
+  const comment = await Comment.findById(commentId);
+
+  if (!comment) {
+    throw new ApiError(404, "comment not found");
+  }
+
+  await Comment.findByIdAndUpdate(
+    commentId,
+    {
+      $set: {
+        content,
+      },
+    },
+    { new: true }
+  );
+
+  console.log("comment updated", comment);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, comment, "comment updated successfully"));
+});
+
+const deleteComment = asyncHandler(async (req, res) => {
+  // TODO: delete a comment
+  // take comment id from params
+  // find comment by id
+  // delete comment
+
+  const { commentId } = req.params;
+
+  if (!commentId) {
+    throw new ApiError(400, "comment id is required");
+  }
+
+  const comment = await Comment.findById(commentId);
+
+  if (!comment) {
+    throw new ApiError(404, "comment not found");
+  }
+
+  await Comment.findByIdAndDelete(commentId);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, comment, "comment deleted successfully"));
+});
+
+export { getVideoComments, addComment, updateComment, deleteComment };
