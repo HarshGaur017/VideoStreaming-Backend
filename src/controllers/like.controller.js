178704d3e0aad030667e3ad1e450dc3fbb5ec9af
Harsh Gaur <harshgaur017@gmail.com>
2025-08-28 18:02:03 +0530

    add like functionality: implement toggle like for videos, comments, and tweets, and add getLikedVideos controller

---
+++ src/controllers/like.controller.js
@@ -0,0 +1,100 @@
+import mongoose, {isValidObjectId} from "mongoose"
+import {Like} from "../models/like.model.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+import { Video } from "../models/video.model.js"
+import { Tweet } from "../models/tweet.model.js"
+
+const toggleVideoLike = asyncHandler(async (req, res) => {
+    const {videoId} = req.params
+    const userId = req.user._id
+    //TODO: toggle like on video
+    // check if video exists
+
+    const video = await Video.findById(videoId);
+
+    if (!video) {
+        throw new ApiError(404, "video not found");
+    }
+
+    const existingLike = await Like.findOne({ video: videoId, likedBy: userId});
+
+    if (existingLike) {
+        await Like.findByIdAndDelete(existingLike._id);
+        return res
+        .status(200)
+        .json(new ApiResponse(200, "video unliked successfully"));
+    } else {
+        await Like.create({ video: videoId, likedBy: userId });
+        return res
+        .status(200)
+        .json(new ApiResponse(200, "video liked successfully"));
+    }
+
+})
+
+const toggleCommentLike = asyncHandler(async (req, res) => {
+    const {commentId} = req.params
+    const userId = req.user._id
+    //TODO: toggle like on comment
+
+    const comment = await Comment.findById(commentId);
+
+    if (!comment) {
+        throw new ApiError(404, "comment not found");
+    }
+
+    const existingLike = await Like.findOne({ comment: commentId, likedBy: userId});
+
+    if (existingLike) {
+        await Like.findByIdAndDelete(existingLike._id);
+        return res
+        .status(200)
+        .json(new ApiResponse(200, comment, "comment unliked successfully"));
+    } else {
+        await Like.create({ comment: commentId, likedBy: userId });
+        return res
+        .status(200)
+        .json(new ApiResponse(200, comment, "comment liked successfully"));
+    }
+
+})
+
+const toggleTweetLike = asyncHandler(async (req, res) => {
+    const {tweetId} = req.params
+    const userId = req.user._id
+    //TODO: toggle like on tweet
+
+    const tweet = await Tweet.findById(tweetId);
+
+    if (!tweet) {
+        throw new ApiError(404, "tweet not found");
+    }
+
+    const existingLike = await Like.findOne({ tweet: tweetId, likedBy: userId});
+
+    if (existingLike) {
+        await Like.findByIdAndDelete(existingLike._id);
+        return res
+        .status(200)
+        .json(new ApiResponse(200, tweet, "tweet unliked successfully"));
+    } else {
+        await Like.create({ tweet: tweetId, likedBy: userId });
+        return res
+        .status(200)
+        .json(new ApiResponse(200, tweet, "tweet liked successfully"));
+    }
+}
+)
+
+const getLikedVideos = asyncHandler(async (req, res) => {
+    //TODO: get all liked videos
+})
+
+export {
+    toggleCommentLike,
+    toggleTweetLike,
+    toggleVideoLike,
+    getLikedVideos
+}
